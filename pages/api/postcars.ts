d8655af7ec0c6cad8bb1a78294fb65fae2bb6952
hirofumi-tokunaga <93700221+hirofumi-tokunaga@users.noreply.github.com>
2022-10-15 15:07:10 +0900

    dda

--- pages/api/postcars.ts
+++ pages/api/postcars.ts
@@ -6,11 +6,12 @@ export default (req: NextApiRequest, res: NextApiResponse) => {
 
 	switch (method) {
 		case "POST": {
-			const { number, name } = req.body;
+			const {id, number, name } = req.body;
 			const sqlite3 = require("sqlite3");
 			const db = new sqlite3.Database("database/cars.db");
 			const result = db.run(
-				"insert into cars(number,name) values(?,?)",
+				"insert into cars(number,name) values(?,?,?)",
+				id,
 				number,
 				name,
 			);
