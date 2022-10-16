4e5fa6ca32fecf39a1c672df2b8b8449e26d2836
hirofumi-tokunaga <93700221+hirofumi-tokunaga@users.noreply.github.com>
2022-10-16 10:08:28 +0900

    dddd

--- pages/api/postcars.tsx
+++ pages/api/postcars.tsx
@@ -1,15 +1,20 @@
 
 import { NextApiRequest, NextApiResponse } from "next"
+import { open } from "sqlite"
 
-export default (req: NextApiRequest, res: NextApiResponse) => {
+export default async (req: NextApiRequest, res: NextApiResponse) => {
 	const method = req.method;
 	console.log(method)
 	switch (method) {
 		case "POST": {
 			const {number, name } = req.body;
 			const sqlite3 = require("sqlite3");
-			const db = new sqlite3.Database("/database/cars.db");
-			const result = db.run(
+
+			// const db = new sqlite3.Database("database/cars.db");
+			const db = await open(
+				{filename:'database/cars.db',driver:sqlite3.Database}
+			)
+			const result = await db.run(
 				"insert into cars(number,name) values(?,?)",
 				number,
 				name,
