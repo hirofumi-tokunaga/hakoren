b03dbcfce9884dbe00f0ffd36a12de306c1f9551
hirofumi-tokunaga <mobinc.tokunaga@gmail.com>
2022-10-17 11:00:51 +0900

    test

--- pages/api/postcars/index.js
+++ pages/api/postcars/index.js
@@ -12,7 +12,7 @@ export default async function handler(req, res) {
 				number,
 				name,
 			);
-			res.status(200).json({ result });
+			res.status(777).json({ result });
 			break;
 		}
 		default: {
