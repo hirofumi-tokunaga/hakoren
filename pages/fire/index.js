0874d884bd5bbfab8ff10af3b65af16a40899c05
hirofumi-tokunaga <93700221+hirofumi-tokunaga@users.noreply.github.com>
2022-10-17 19:21:53 +0900

    tes

--- pages/fire/index.js
+++ pages/fire/index.js
@@ -1,4 +1,4 @@
-import {useState,useEffect} from 'react'
+import {useEffect} from 'react'
 import Layout from 'components/layout'
 // import 'components/fire'
 import {initializeApp} from 'firebase/app'
