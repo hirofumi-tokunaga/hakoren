470c9b5a84b469849db823a53d79f4d8dbfdd167
hirofumi-tokunaga <mobinc.tokunaga@gmail.com>
2022-11-08 17:02:16 +0900

    login

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,12 @@
 import '../styles/globals.scss'
-import Layout from 'components/layout';
+import Layout from 'components/layout'
+import { useAuthState } from 'hooks/useAuthState'
 
 function MyApp({ Component, pageProps }) {
 	return (
 		<Layout>
 			< Component {...pageProps } />
 		</Layout>
-
 	)
 }
 
--- pages/index.js
+++ pages/index.js
@@ -3,18 +3,25 @@ import Meta from 'components/meta'
 import MainHead from 'components/mainhead'
 import { SignInOrOutButton } from 'components/SignInOrOutButton'
 import { useAuthState } from 'hooks/useAuthState'
+import { SignInForm } from "../components/SignInForm"
 
 export default function Home() {
 	const login = useAuthState()
 	console.log(login)
 	return (
-		<>
-			<Meta />
-			<Box>
-				<MainHead title="トップページ" />
-			</Box>
-			<SignInOrOutButton />
-
-		</>
+		login.isSignedIn ? (
+			<>
+				<Meta />
+				<Box>
+					<MainHead title="トップページ" />
+				</Box>
+				<SignInOrOutButton />
+			</>
+		) : (
+			<>
+				<MainHead title="ログインページ" />
+				<SignInForm />
+			</>
+		)
 	)
 }
