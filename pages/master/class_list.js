057e23f5e583d91c295156ce7ff14db2b37e7ec3
hirofumi-tokunaga <93700221+hirofumi-tokunaga@users.noreply.github.com>
2022-11-25 23:38:03 +0900

    search 終わり

--- pages/master/class_list.js
+++ pages/master/class_list.js
@@ -6,7 +6,6 @@ import TextField from '@mui/material/TextField'
 import Button from '@mui/material/Button'
 import Box from '@mui/material/Box'
 
-
 import MainHead from 'components/mainhead'
 import DeleteModal from 'pages/modal/delete_modal'
 import Loading from 'components/loading'
@@ -153,6 +152,9 @@ export default function ClassList() {
 								画像
 							</div>
 							<div className={styles.th}>
+								紹介文
+							</div>
+							<div className={styles.th}>
 								オプション
 							</div>
 							<div className={styles.th}>
@@ -235,6 +237,16 @@ export default function ClassList() {
 												</div>
 												<div className={styles.td}>
 														<img src={item.image} style={{ height:"100px"}} />
+												</div>
+												<div className={styles.td}>
+													<Link href={"/master/class_text"} as={`/master/class_text/${item.id}`}>
+														<Button
+															variant="outlined"
+															className={styles.btn}
+														>
+															紹介文設定
+														</Button>
+													</Link>
 												</div>
 											</>
 										)}
--- pages/search.js
+++ pages/search.js
@@ -7,11 +7,10 @@ import InputLabel from '@mui/material/InputLabel'
 import FormControl from '@mui/material/FormControl'
 import RadioGroup from '@mui/material/RadioGroup';
 
-import Loading from 'components/loading'
 import DatePicker_Custom from 'components/datepicker-custom'
 import TimePicker from 'components/timepicker'
 
-import styles from 'styles/reservation.module.scss'
+import styles from 'styles/search.module.scss'
 
 export default function Search() {
 	const Today = new Date()
@@ -19,12 +18,11 @@ export default function Search() {
 	const [endDate, setEndDate] = useState(Today)
 	const [startTime, setStartTime] = useState("08:30")
 	const [endTime, setEndTime] = useState("20:30")
-	const [loading, setLoading] = useState(false)
 	const [scheduleOk, setScheduleOk] = useState(false)
 	const [carList, setCarList] = useState([])
+	const [optionList,setOptionList] = useState([])
 	const [okClass, setOkClass] = useState([])
 	const [classList,setClassList] = useState([])
-	const [selectCarId, setSelectCarId] = useState()
 	const [isSearch, setIsSearch] = useState(false)
 
 	const transDate = (date) => {
@@ -39,7 +37,6 @@ export default function Search() {
 		let currentEnd = Number(transDate(endDate))
 		await getDb('bookinginfo').then((bookingInfo) => {
 			let okCarList = []
-			let newItems = []
 			carList.forEach((car) => {
 				let carOK = true
 				bookingInfo.forEach((info) => {
@@ -68,6 +65,7 @@ export default function Search() {
 		async function init() {
 			setCarList(await getDb('carlist'))
 			setClassList(await getDb('class'))
+			setOptionList(await getDb('option'))
 		}
 		init();
 	}, [])
@@ -79,93 +77,105 @@ export default function Search() {
 		init();
 	}, [startDate, endDate])
 	return (
-		<>
-			<Loading loading={loading} />
-			<Box className={styles.container}>
-
-				<Box className={styles.outline}>
-					<h2>レンタカー最安検索</h2>
-					<Box className={styles.form}>
-						<Box className={styles.schedule} >
-							<Box className={styles.box}>
-								<FormControl className={styles.day}>
-									<InputLabel className="input-label" shrink={true} name="startDate">出発日</InputLabel>
-									<DatePicker_Custom date={startDate} setDate={setStartDate} />
-								</FormControl>
-								<FormControl className={styles.time} >
-									<InputLabel className="input-label" shrink={true} name="startTime">出発時刻</InputLabel>
-									<TimePicker time={startTime} setTime={setStartTime} />
-								</FormControl>
-							</Box>
-							<Box className={styles.box}>
-								<FormControl className={styles.day} >
-									<InputLabel className="input-label" shrink={true} name="endDate">返却日</InputLabel>
-									<DatePicker_Custom date={endDate} setDate={setEndDate} />
-								</FormControl>
-								<FormControl className={styles.time}>
-									<InputLabel className="input-label" shrink={true} name="endTime">返却時刻</InputLabel>
-									<TimePicker time={endTime} setTime={setEndTime} />
-								</FormControl>
-							</Box>
-
+		<Box className={styles.container}>
+			<Box className={styles.outline}>
+				<h2>レンタカー最安検索</h2>
+				<Box className={styles.form}>
+					<Box className={styles.schedule} >
+						<Box className={styles.box}>
+							<FormControl className={styles.day}>
+								<InputLabel className="input-label" shrink={true} name="startDate">出発日</InputLabel>
+								<DatePicker_Custom date={startDate} setDate={setStartDate} />
+							</FormControl>
+							<FormControl className={styles.time} >
+								<InputLabel className="input-label" shrink={true} name="startTime">出発時刻</InputLabel>
+								<TimePicker time={startTime} setTime={setStartTime} />
+							</FormControl>
 						</Box>
-						<Button
-							className={styles.btn}
-							variant="contained"
-							sx={{ mb: 2 }}
-							onClick={() => {
-								handleDateCheck()
-								setIsSearch(true)
-							}}
-						>在庫検索</Button>
-					</Box>
-				</Box>
-				<p style={scheduleOk ? { color: "#0000ff" } : { color: "#ff0000" }}>{isSearch && (scheduleOk ? "以下の在庫が在ります" : "条件に一致する在庫がありません")}</p>
-				<RadioGroup>
-					<ul className={styles.cardata}>
-						{isSearch && (okClass?.map((item,i) => {
-							const classData = classList?.filter((data) => data.name === item)[0]
-							return (
-								<li key = { i } className="flex">
-									{classData && (
-										<>
-											<div className={styles.left}>
-												<div className={styles.c_name}>
-													{classData.name}クラス
-												</div>
-												<div className={styles.c_image}>
-													<img src={classData.image}/>
-												</div>
-												<div className={styles.c_info}>
-													<div className="flex">
-														<div>車型</div>
-														<div>{classData.car}</div>
-													</div>
-													<div className="flex">
-														<div>定員</div>
-														<div>{classData.capacity} 名</div>
-													</div>
-												</div>
-											</div>
-											<div className={styles.right}>
-												<div className={styles.price_title}>当日料金</div>
-												<div className={styles.price_wrap}><span className={styles.bunner}>WEB価格</span><span className={styles.price}>{Number(classData.price).toLocaleString()}</span>円～（税込）</div>
-												<Button
-													variant="contained"
-												>
-													詳細・お見積りへ
-												</Button>
-											</div>
-										</>
-									)}
-								</li>
-							)
-						}
-						))}
-					</ul>
-				</RadioGroup>
-			</Box>
+						<Box className={styles.box}>
+							<FormControl className={styles.day} >
+								<InputLabel className="input-label" shrink={true} name="endDate">返却日</InputLabel>
+								<DatePicker_Custom date={endDate} setDate={setEndDate} />
+							</FormControl>
+							<FormControl className={styles.time}>
+								<InputLabel className="input-label" shrink={true} name="endTime">返却時刻</InputLabel>
+								<TimePicker time={endTime} setTime={setEndTime} />
+							</FormControl>
+						</Box>
 
-		</>
+					</Box>
+					<Button
+						className={styles.btn}
+						variant="contained"
+						sx={{ mb: 2 }}
+						onClick={() => {
+							handleDateCheck()
+							setIsSearch(true)
+						}}
+					>在庫検索</Button>
+				</Box>
+			</Box>
+			<p style={scheduleOk ? { color: "#0000ff" } : { color: "#ff0000" }}>{isSearch && (scheduleOk ? "以下の在庫が在ります" : "条件に一致する在庫がありません")}</p>
+			<RadioGroup>
+				<ul className={styles.cardata}>
+					{isSearch && (okClass?.map((item,i) => {
+						const classData = classList?.filter((data) => data.name === item)[0]
+						return (
+							<li key = { i } className="flex">
+								{classData && (
+									<>
+										<div className={styles.left}>
+											<div className={styles.c_name}>
+												{classData.name}クラス
+											</div>
+											<div className={styles.c_image}>
+												<img src={classData.image}/>
+											</div>
+											<div className={styles.c_info}>
+												<div className="flex">
+													<div>車型</div>
+													<div>{classData.car}</div>
+												</div>
+												<div className="flex">
+													<div>定員</div>
+													<div>{classData.capacity} 名</div>
+												</div>
+											</div>
+										</div>
+										<div className={styles.center}>
+											<div className={styles.option}>
+												<div>標準装備</div>
+												<div>
+													{classData.basic_option.map((item,i) => {
+														return (
+															<span>
+																{optionList.filter((item2) => item2.id === item)[0]?.name}
+															</span>
+														)
+													})}
+												</div>
+											</div>
+											<div className={styles.text}>
+												{classData.text}
+											</div>	
+										</div>
+										<div className={styles.right}>
+											<div className={styles.price_title}>当日料金</div>
+											<div className={styles.price_wrap}><span className={styles.bunner}>WEB価格</span><span className={styles.price}>{Number(classData.price).toLocaleString()}</span>円～（税込）</div>
+											<Button
+												variant="contained"
+											>
+												詳細・お見積りへ
+											</Button>
+										</div>
+									</>
+								)}
+							</li>
+						)
+					}
+					))}
+				</ul>
+			</RadioGroup>
+		</Box>
 	)
 }
